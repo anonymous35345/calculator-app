0c4c0c8627136f4cabf6efa19da0c803fad1caeb
anonyous35345 <anon112233anony@gmail.com>
2026-01-04 10:59:33 +1300

    figuring out array and string methods

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ function textcalculating(){
      btn.addEventListener("click" , () => {
         textcal =  textcal+btn.dataset.value
         console.log(textcal)
+        let resault = parseFloat(textcal);
+        console.log(resault);
+        
      })
     })
 }
 textcalculating();
   
+
+
+
+let username = "15*88+66"
+rere = username.replaceAll("*" , "multiplicatiopn")
+console.log(rere);
+
